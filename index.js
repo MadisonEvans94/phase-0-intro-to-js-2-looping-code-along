f55b431840f7ef8be2cd8762bb36da6f3d226889
Madison Evans <m.corbinevans@gmail.com>
2023-01-09 20:53:05 -0700

    commit tree 6eec5a84a15469897844c6f641a8ce2361ede727

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(nameArray, eventName) {
+	let cardArray = [];
+	for (let i = 0; i < nameArray.length; i++) {
+		cardArray.push(
+			`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`
+		);
+	}
+	return cardArray;
+}
+
+function countDown(num) {
+	let counter = 0;
+	for (let i = 0; i < num; i++) {
+		console.log(num - counter);
+		counter++;
+	}
+	console.log(num - counter);
+}
